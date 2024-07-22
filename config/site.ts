27daa8ae8c1b90ad89357d2a4f42e62bea414af6
PAPATIRI <syariftaufik11@gmail.com>
2024-07-23 00:13:25 +0700

    add site header and site configuration

---
+++ config/site.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  name: "BlogUh",
+  url: "https://example.com",
+  description: "Nexjs 14 with tailwindcss typescript shadcn and velite",
+  author: "zancc",
+  links: {
+    instagram: "https://www.instagram.com/zancc_/",
+    tiktok: "https://www.tiktok.com/@syarif_th_",
+    personalSite: "https://syarifth.my.id",
+    github: "https://github.com/PAPATIRI",
+    twitter: "https://x.com/Syarif_T_H",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
