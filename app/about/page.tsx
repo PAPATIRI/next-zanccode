d53fdf8b50a0c84eb7a4519ad0962fa7f9083611
PAPATIRI <syariftaufik11@gmail.com>
2024-07-23 22:46:18 +0700

    add about page and fix styling homepage

---
+++ app/about/page.tsx
@@ -0,0 +1,49 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { siteConfig } from "@/config/site";
+
+export default async function AboutPage() {
+  return (
+    <div className="container max-w-6xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className="flex-1 space-x-4">
+          <h1 className="inline-block font-black text-4xl lg:text-5xl">
+            About Me
+          </h1>
+        </div>
+      </div>
+      <hr className="my-8" />
+      <div className="flex flex-col md:flex-row gap-8 items-center md:items-start">
+        <div className="min-w-48 max-w-48 flex flex-col gap-2">
+          <Avatar className="h-48 w-48">
+            <AvatarImage src="/avatar.png" alt={siteConfig.author} />
+            <AvatarFallback>zancc</AvatarFallback>
+          </Avatar>
+          <h2 className="text-2xl font-bold text-center break-words">
+            {siteConfig.author}
+          </h2>
+          <p className="text-muted-foreground text-center break-words">
+            Programmer Ganteng & Intelek
+          </p>
+        </div>
+        <div>
+          <p className="text-muted-foreground text-lg py-2">
+            seorang pria tampan kelahiran pedalaman Kalimantan Barat. dengan
+            nama syarif taufik hidayatulloh (nama menurut akta kelahiran), yang
+            berarti kurang lebih "petunjuk atau hidayah dari Allah yang mulia"
+            dayummmmmm so gewd my name, thnx ayahanda dan ibunda koeh.
+          </p>
+          <p className="text-muted-foreground text-lg py-2">
+            senang menyesatkan diri belajar hal baru di dunia programming atau
+            software development. membangun banyak side project tapi tidak di
+            selesaikan. detective conan fans meskipun masih belum lanjut
+            marathon sejak di episode 1033. ini website personal blog ke 4 saya.
+            website personal blog pertama dibangun dengan wordpress, kemudian
+            yang ke dua dibangun dengan nextjs 10, kemudian yang ke tiga
+            dibangun dengan laravel, dan website personal blog terbaru ini
+            dibangun dengan nextjs 14
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
-        <div className="container flex flex-col gap-4 text-center">
+        <div className="container flex flex-col gap-8 text-center">
           <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
             Hello, I&apos;m Syarif
           </h1>
@@ -28,7 +28,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-60">
+      <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-52">
         <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
           Latest Posts
         </h2>
