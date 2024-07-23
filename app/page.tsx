1e55d9f21d760ddd43a6ddf2e90c2d2e14b3a627
PAPATIRI <syariftaufik11@gmail.com>
2024-07-23 22:16:42 +0700

    change homepage view

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,50 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn, sortPosts } from "@/lib/utils";
+import { posts } from "#site/content";
+import Link from "next/link";
+import { PostItem } from "@/components/post-item";
+
 export default function Home() {
-  return <section>Hello World</section>;
+  const latesPost = sortPosts(posts).slice(0, 5);
+
+  return (
+    <>
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
+        <div className="container flex flex-col gap-4 text-center">
+          <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
+            Hello, I&apos;m Syarif
+          </h1>
+          <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
+            Welcome to my <q className="inline font-bold italic">dear diary</q>,
+            where i write all my problems down clearly so it is half solved
+          </p>
+          <div className="flex flex-col gap-4 justify-center sm:flex-row">
+            <Link
+              href={"/blog"}
+              className={cn(buttonVariants({ size: "lg" }), "w-full sm:w-fit")}
+            >
+              Read The Masterpiece
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section className="container max-w-4xl py-6 lg:py-10 flex flex-col space-y-6 mt-60">
+        <h2 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-center">
+          Latest Posts
+        </h2>
+        <ul className="flex flex-col">
+          {latesPost.map((post) => (
+            <li key={post.slug} className="first:border-t first:border-border">
+              <PostItem
+                slug={post.slug}
+                title={post.title}
+                description={post.description}
+                date={post.date}
+              />
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
 }
